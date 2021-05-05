c6520a375f946158e62024b99510f815c54822e9
Vladimir Shestakov <vlad-shestakov@mail.ru>
2021-05-05 16:39:09 +0300

    Изменения в описании бота

--- vlad-shestakov-bot.py
+++ vlad-shestakov-bot.py
@@ -16,6 +16,7 @@ def send_welcome(message):
     bot.reply_to(message, """\
 Hi there, I am EchoBot.
 I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
+\nЯ использую команды start / help / привет
 """+\
                  f'\nВаше имя - {message.from_user.first_name}')
 
