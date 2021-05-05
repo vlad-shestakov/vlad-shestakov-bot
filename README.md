2c91f4dc0b5cdd4183e75ea63a5f3fe6f7cd382f
Vladimir Shestakov <vlad-shestakov@mail.ru>
2021-05-05 16:01:01 +0300

    Создан бот

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # vlad-shestakov-bot
 Test telegram bot
+
+Name - vlad-shestakov-bot
+
+Команды:
+* help / start
+* привет
---
+++ vlad-shestakov-bot.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+
+# This is a simple echo bot using the decorator mechanism.
+# It echoes any incoming text messages.
+
+import telebot
+
+API_TOKEN = '1722864364:AAEHm8b1dICPIk13Qp5Ul3YlOILcUjjzpZE'
+
+bot = telebot.TeleBot(API_TOKEN)
+
+
+# Handle '/start' and '/help'
+@bot.message_handler(commands=['help', 'start'])
+def send_welcome(message):
+    bot.reply_to(message, """\
+Hi there, I am EchoBot.
+I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
+"""+\
+                 f'Ваше имя - {message.from_user.first_name}')
+
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text.lower() == 'привет':
+        bot.send_message(message.from_user.id, 'Привет!')
+    else:
+        bot.send_message(message.from_user.id, 'Не понимаю, что это значит.')
+
+
+# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
+@bot.message_handler(func=lambda message: True)
+def echo_message(message):
+    bot.reply_to(message, message.text)
+
+bot.polling(none_stop=True)
