8f778dbf5004bca3307958558340bb9eadf32895
Vladimir Shestakov <vlad-shestakov@mail.ru>
2021-05-05 16:58:32 +0300

    Исправлено описание help

--- vlad-shestakov-bot.py
+++ vlad-shestakov-bot.py
@@ -14,10 +14,8 @@ bot = telebot.TeleBot(API_TOKEN)
 @bot.message_handler(commands=['help', 'start'])
 def send_welcome(message):
     bot.reply_to(message, """\
-Hi there, I am EchoBot.
-I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
-\nЯ использую команды start / help / привет
-"""+\
+Привет, я бот Владмира Шестакова\n
+Я использую команды start / help / привет"""+\
                  f'\nВаше имя - {message.from_user.first_name}')
 
 
